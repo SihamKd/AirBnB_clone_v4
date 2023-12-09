63957133060fbe92048d31be5bad8547bebacb08
Siham <khaddou.siham@gmail.com>
2023-12-09 18:47:16 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,13 @@ from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+# Updated CORS configuration
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
